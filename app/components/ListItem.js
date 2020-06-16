1a24d556ed1eae7fbe4b76e19b20496926091e38
rabah babaci <4rabah@gmail.com>
2020-06-16 09:27:53 -0700

    create the ListItem component

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { View, Image, TouchableHighlight, StyleSheet } from "react-native";
 import Swipeable from "react-native-gesture-handler/Swipeable";
+
 import colors from "../config/colors";
+import AppText from "../components/AppText";
 
 export default function ListItem({
   image,
@@ -17,6 +19,11 @@ export default function ListItem({
         <View style={styles.container}>
           {IconComponent}
           {image && <Image source={image} style={styles.image} />}
+
+          <View style={styles.infoContainer}>
+            <AppText style={styles.title}>{title}</AppText>
+            {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
+          </View>
         </View>
       </TouchableHighlight>
     </Swipeable>
